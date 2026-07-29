f8408fccd555e0ed6bec8ca6c3d4a11e44afed13
hector-romao <147180823+hector-romao@users.noreply.github.com>
2026-07-29 15:37:54 -0300

    Update rescale_tree_I-type.R

--- PGLS_analysis/rescale_tree_I-type.R
+++ PGLS_analysis/rescale_tree_I-type.R
@@ -10,8 +10,8 @@ library(OUwie)
 ############################ Read tree and dataset #############################
 ################################################################################
 
-tree <- read.tree(here("data", "ultrametric_tree.nwk"))
-dat  <- read.csv(here("data","i_type_input_data.csv"), stringsAsFactors = FALSE)
+tree <- read.tree(here("PGLS_analysis","data", "ultrametric_tree.nwk"))
+dat  <- read.csv(here("PGLS_analysis","data","i_type_input_data.csv"), stringsAsFactors = FALSE)
 
 ################################################################################
 ############################ Check species matching ############################
